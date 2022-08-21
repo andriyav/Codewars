d495bd2eec53eae7fcc08a0b80fd5f8dd6a1f7d1
andriyav@hotmail.com <andriyav@hotmail.com>
2022-08-22 00:14:53 +0300

    sum_of_intervals 2

--- Sum_of_Intervals.py
+++ Sum_of_Intervals.py
@@ -1,10 +1,13 @@
 def sum_of_intervals(intervals):
-    while len(intervals) > 1:
+    while len(intervals) > 2:
         a = intervals[0]
         b = intervals[1]
         overlap = max(0, min(a[1], b[1]) - max(a[0], b[0]))
         total_len = (a[1]-a[0]) + (b[1]-b[0])
         new_intervals = [[min(a[0], b[0]), max(a[1], b[1])]]
+        intervals.pop(0)
+        intervals.pop(0)
+        new_intervals.append(intervals[0])
         intervals = new_intervals.copy()
     print(max(a[1], b[1]))
     print(min(a[0], b[0]))
