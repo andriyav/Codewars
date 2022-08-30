b0635905c95a1c284b7b76057d51ebfbf5b1408c
andriyav@hotmail.com <andriyav@hotmail.com>
2022-08-30 23:51:18 +0300

    problem: overlaped items cannot meet each others 2

--- Sum_of_Intervals.py
+++ Sum_of_Intervals.py
@@ -6,24 +6,31 @@ def sum_of_intervals(intervals):
     overlap_list = []
     result_list = []
     unique = []
-    while len(intervals) > 1:
-        a = intervals[0]
-        b = intervals[1]
-        if a != b:
-            overlap = max(0, min(a[1], b[1]) - max(a[0], b[0]))
-            if overlap > 0:
-                new_intervals = [min(a[0], b[0]), max(a[1], b[1])]
-                intervals.append(new_intervals)
-                overlap_list.append(new_intervals)
-                intervals.pop(0)
-                intervals.pop(0)
-            else:
-                non_overlap_list.append(b)
-                non_overlap_list.append(a)
-                intervals.append(a)
-                intervals.append(b)
-                intervals.pop(0)
-                intervals.pop(0)
+    n = 0
+    k = 0
+    while len(intervals) > n:
+
+        while len(intervals) > k + 1:
+
+            a = intervals[k]
+            b = intervals[n]
+            if a != b:
+                overlap = max(0, min(a[1], b[1]) - max(a[0], b[0]))
+                if overlap > 0:
+                    new_intervals = [min(a[0], b[0]), max(a[1], b[1])]
+                    intervals.append(new_intervals)
+                    overlap_list.append(new_intervals)
+                    intervals.pop(0)
+                    intervals.pop(0)
+                else:
+                    non_overlap_list.append(b)
+                    non_overlap_list.append(a)
+                    intervals.append(a)
+                    intervals.append(b)
+                    intervals.pop(0)
+                    intervals.pop(0)
+            k += 1
+        n += 1
     print(intervals, 'intervals')
     print(non_overlap_list, 'non_overlap_list')
     for m in non_overlap_list:
@@ -45,9 +52,8 @@ def sum_of_intervals(intervals):
 
 
 sum_of_intervals([
-   [1,5],
-   [10, 20],
-   [1, 6],
-   [16, 19],
-   [5, 11]
-])
+    [1, 5],
+    [10, 20],
+    [1, 6],
+    [16, 19],
+    [5, 11]
