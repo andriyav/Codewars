54ec8331bb48f538c28dc70375acce7ff1dea2db
andriyav@hotmail.com <andriyav@hotmail.com>
2022-09-01 20:31:56 +0300

    done

--- Sum_of_Intervals.py
+++ Sum_of_Intervals.py
@@ -1,59 +1,25 @@
 def sum_of_intervals(intervals):
-    result = 0
     new_intervals = []
-    intervalss = intervals.copy()
-    non_overlap_list = []
-    overlap_list = []
-    result_list = []
+    cycle = 10
+    result = 0
     unique = []
-    n = 0
-    k = 0
-    while len(intervals) > n:
-
-        while len(intervals) > k + 1:
-
-            a = intervals[k]
-            b = intervals[n]
-            if a != b:
-                overlap = max(0, min(a[1], b[1]) - max(a[0], b[0]))
-                if overlap > 0:
-                    new_intervals = [min(a[0], b[0]), max(a[1], b[1])]
-                    intervals.append(new_intervals)
-                    overlap_list.append(new_intervals)
-                    intervals.pop(0)
-                    intervals.pop(0)
-                else:
-                    non_overlap_list.append(b)
-                    non_overlap_list.append(a)
-                    intervals.append(a)
-                    intervals.append(b)
-                    intervals.pop(0)
-                    intervals.pop(0)
-            k += 1
-        n += 1
-    print(intervals, 'intervals')
-    print(non_overlap_list, 'non_overlap_list')
-    for m in non_overlap_list:
-        a = intervals[0]
-        b = m
-        overlap = max(0, min(a[1], b[1]) - max(a[0], b[0]))
-        new_intervals = [min(a[0], b[0]), max(a[1], b[1])]
-        if overlap > 0:
-            intervals.append(new_intervals)
-            result_list.append(new_intervals)
-        else:
-            result_list.append(m)
-
-        result_list.append(intervals[0])
-    for i in result_list:
-        if i not in unique:
-            unique.append(i)
-    print(unique)
-
-
-sum_of_intervals([
-    [1, 5],
-    [10, 20],
-    [1, 6],
-    [16, 19],
-    [5, 11]
+    while cycle > 1:
+        for a in intervals:
+            for b in intervals:
+                if a != b:
+                    overlap = max(0, min(a[1], b[1]) - max(a[0], b[0])) # число перекриття двох інтервалів
+                    if overlap > 0:
+                        new_intervals = [min(a[0], b[0]), max(a[1], b[1])] # заміна двох інтервалів занальним
+                        intervals.insert(0, new_intervals) # додавання нового інтервалу на початок листа
+                        intervals.pop(intervals.index(a)) # видалення двох злитів інтервалів
+                        intervals.pop(intervals.index(b))
+                        break
+        cycle -= 1
+    for u in intervals: # видалення однакових інтервалів
+        if u not in unique:
+            unique.append(u)
+    for s in unique: # розрахунок довжини інтервалів
+        item_len = s[1] - s [0]
+        result += item_len
+    print(result)
+sum_of_intervals([[1, 5], [1, 5], [30, 34], [35, 37], [35, 38], [2, 3], [4, 8]])
