917074047d5f4682a43db72e2f2c4a8dbda431a1
andriyav@hotmail.com <andriyav@hotmail.com>
2022-10-03 09:21:29 +0300

    # should equal [27380220974279848, 7, 0] check it!

--- Find_the_smallest.py
+++ Find_the_smallest.py
@@ -1,4 +1,52 @@
+import numpy as np
 def smallest(n):
+    n_str_list = list(str(n))
+    n_int_list = list(map(int, n_str_list))
+    print(n)
+    k = 0
+    for c in range(0, 10):
+        len_list = len(n_int_list) - 1
+        while len_list > 0:
+            f = n_int_list[len_list]
+            if n_int_list[len_list] == c:
+                n_int_list.pop(len_list)
+                n_int_list.insert(0, c)
+                k = 1
+                break
+            len_list -= 1
+        if k == 1:
+            break
+    if n_int_list[0] == 0:
+        n_int_list.pop(0)
+    indexes = int(''.join(list(map(str, n_int_list))))
+    result1 = [len_list, 0]
+    result1.insert(0, indexes)
+    print(result1)
+
+
+    # метод 2
+    n_str_list = list(str(n))
+    n_int_list = list(map(int, n_str_list))
+    m = 0
+    if n_int_list[1] == 0:
+        k = n_int_list.pop(0)
+        m = 0
+        for d in n_int_list:
+            if k < d:
+                n_int_list.insert(m, k)
+                break
+            m += 1
+    if n_int_list[0] == 0:
+        n_int_list.pop(0)
+    # result2 = int(''.join(list(map(str, n_int_list))))
+    # indexes = [0, m]
+    # print(indexes)
+    indexes = int(''.join(list(map(str, n_int_list))))
+    result2 = [0, m]
+    result2.insert(0, indexes)
+    print(result2)
+
+    # метод 3
     n_str_list = list(str(n))
     n_int_list = list(map(int, n_str_list))
     deleted_items = []
@@ -14,21 +62,63 @@ def smallest(n):
     min_l = min(n_int_list)
     while True: # пошук найменшого другого значення з кінця.
         if n_int_list[i] == min_l:
+            print(i)
             break
         i -= 1
     n_int_list.pop(i)
     n_int_list.insert(0, min_l)
-    len_del = len(deleted_items)
     resutlt_list = deleted_items + n_int_list
+    ind1 = len(deleted_items)
     if resutlt_list[0] == 0:
         resutlt_list.pop(0)
-    result = int(''.join(list(map(str, resutlt_list))))
-    if len(str(result)) != len(str(n)) and str(n)[1] == '0':
-        result = [result, len_del, (i + len_del)]
-    else:
-        result = [result, (i+len_del), len_del]
+    # result3 = int(''.join(list(map(str, resutlt_list))))
+    # indexes = [i, 0]
+    # print(indexes)
+    indexes = int(''.join(list(map(str, resutlt_list))))
+    result3 = [i+1, ind1]
+    result3.insert(0, indexes)
+    print(result3)
 
+    # метод 4
+    n_str_list = list(str(n))
+    n_int_list = list(map(int, n_str_list))
+    k = 0
+    for c in range(9, 0, -1):
+        len_list = len(n_int_list)
+        m = 0
+        while len_list > m:
+            f = n_int_list[m]
+            if n_int_list[m] == c:
+                n_int_list.pop(m)
+                l = len(n_int_list)
+                n_int_list.insert(l, c)
+                k = 1
+                break
+            m += 1
+        if k == 1:
+            break
+    if n_int_list[0] == 0:
+        n_int_list.pop(0)
+    # indexes = [m, l]
+    # print(indexes)
+    # result4 = int(''.join(list(map(str, n_int_list))))
+    indexes = int(''.join(list(map(str, n_int_list))))
+    result4 = [m, l]
+    result4.insert(0, indexes)
+    print(result4)
+    print(result1, result2, result3, result4)
+    a = [result1, result2, result3, result4]
 
+    print(a)
+    a = sorted(a, key=lambda a_entry: a_entry[0])
+    print(a)
+    result = a[0]
     print(result)
+    return result
 
-smallest(269045)
+    # result = min(result1, result2, result3, result4)
+
+    # print(result)
+
+smallest(273802200974279848)
+# should equal [27380220974279848, 7, 0]
