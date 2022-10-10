418b7a710712e9aa5339b3193e4d1eb241fa78c5
andriyav@hotmail.com <andriyav@hotmail.com>
2022-10-11 00:02:10 +0300

    done

--- Scramblies.py
+++ Scramblies.py
@@ -1,10 +1,22 @@
+'''Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+    Only lower case letters will be used (a-z). No punctuation or digits will be included.
+    Performance needs to be considered.
+'''
+import string
 def scramble(s1, s2):
-    for i in s2:
-        if i in s1:
+    s1_list = list(s1)
+    s2_list = list(s2)
+    for i in list(string.ascii_lowercase):
+        n1 = s1_list.count(i)
+        n2 = s2_list.count(i)
+        if n1 >= n2:
             continue
         else:
-            print('False')
             return False
-    print('True')
     return True
-scramble("pgzwvlpcbshrbavy", "svghbvbpwywl")
+
+
+scramble("acgyfgumwea", "awcmuayyf")
