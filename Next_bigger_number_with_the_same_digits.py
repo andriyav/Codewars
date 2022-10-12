4f97fa4e54f97ec39cb3dde3ff0bc05053550ca3
andriyav@hotmail.com <andriyav@hotmail.com>
2022-10-13 00:34:33 +0300

    day2

--- Next_bigger_number_with_the_same_digits.py
+++ Next_bigger_number_with_the_same_digits.py
@@ -6,32 +6,37 @@ def next_bigger(n):
     min_list = n_int_list_muteable.copy()
     result6 = []
     #  переміщенні кожної цифри з ліва на право
-    for g1 in range(0, range_len):
+
+    for g1 in range(range_len - 1, 0, -1):
         n_int_list_muteable = n_int_list.copy()
-        for g2 in range(2, range_len + 1):
-            n_int_list_muteable.insert(g2 + g1, n_int_list[g1])
-            n_int_list_muteable.pop(g2 + g1 - 2)
-            if int(''.join(list(map(str, n_int_list_muteable)))) > int(
-                    ''.join(list(map(str, min_list)))) :  # зі всього переліку залишається найменше число
-                min_list = int(''.join(list(map(str, n_int_list_muteable))))
-                print(min_list)
-                return(min_list)
-            if g1 + g2 >= range_len:
+        c = 0
+        for g2 in range(range_len - 1, 0, -1):
+            n_int_list_muteable.insert(g1 + 1 + c, n_int_list[g1 - 1])
+            c += 1
+            n_int_list_muteable.pop(g1 + c - 2)
+            # if int(''.join(list(map(str, n_int_list_muteable)))) > int(
+            #         ''.join(list(map(str, min_list)))) :  # зі всього переліку залишається найменше число
+            #     min_list = int(''.join(list(map(str, n_int_list_muteable))))
+            #     print(min_list)
+            #     return(min_list)
+
+            if g1 + c >= range_len:
                 break
 
-    g = 0
-    for g1 in range(range_len, -1, -1):
-        n_int_list_muteable = n_int_list.copy()
-        for g2 in range(range_len, 1 + g, -1):
-            n_int_list_muteable.insert(g2 - 2 - g, n_int_list[g1 - 1])
-            n_int_list_muteable.pop(g2 - g)
-            if int(''.join(list(map(str, n_int_list_muteable)))) > n:
-                max_list = int(''.join(list(map(str, n_int_list_muteable))))
-                print(max_list)
-                return max_list
-        g += 1
-    print(-1)
-    return -1
+    # g = 0
+    # for g1 in range(range_len, -1, -1):
+    #     n_int_list_muteable = n_int_list.copy()
+    #     for g2 in range(range_len, 1 + g, -1):
+    #         n_int_list_muteable.insert(g2 - 2 - g, n_int_list[g1 - 1])
+    #         n_int_list_muteable.pop(g2 - g)
+    #         if int(''.join(list(map(str, n_int_list_muteable)))) > n:
+    #             max_list = int(''.join(list(map(str, n_int_list_muteable))))
+    #             print(max_list)
+    #             return max_list
+    #     g += 1
+    # print(-1)
+    # return -1
+
 
 next_bigger(1234567890)
 # 1234567908
