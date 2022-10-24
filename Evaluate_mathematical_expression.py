a0b16ce09f560e3fd58539cfc6e90403596bcefa
andriyav@hotmail.com <andriyav@hotmail.com>
2022-10-24 20:16:29 +0300

    day5   # Зробити заміну у виразі без дужок

--- Evaluate_mathematical_expression.py
+++ Evaluate_mathematical_expression.py
@@ -4,11 +4,12 @@ import re
 
 def calc(expression):
     while True:
-        pattern = r'(-?\d+.?\d? [/*+-] -?\d+.?\d?)'
+        pattern = r'(\(-?\d+.?\d* [/*+-] -?\d+.?\d*\))'
         re_expression = re.finditer(pattern, expression)
         re_exp_list = list(re_expression)
         expression_list = list(expression)
-        print(re_expression)
+        print(re_exp_list)
+
         if re_exp_list != []:
             print(re_exp_list[0].span())
             a = 0
@@ -25,13 +26,14 @@ def calc(expression):
         else:
             break
     print(expression, 'simpled')
-    # operations(expression)
+    operations(expression)
 
 
 def operations_in_brackets(expr):
     print(expr)
-    pattern = r'(-?\d+.?\d?) ([*/+-]) (-?\d+.?\d?)'
+    pattern = r'(-?\d+.?\d*) ([*/+-]) (-?\d+.?\d*)\)'
     re_expression = re.findall(pattern, expr)
+    print(list(re_expression))
     print(re_expression[0], 're[01]')
     a, act, b = re_expression[0]
     print(a, act, b)
@@ -49,13 +51,23 @@ def operations_in_brackets(expr):
     return c
 
 
-
-
 def operations(expr):
-    pattern = r'(-?\d+) ([*/]) (-?\d+)'
-    re_expression = re.findall(pattern, expr)
-    print(re_expression[0])
-    a, act, b = re_expression[0]
+    pattern = r'(-?\d+.?\d*) ([*/]) (-?\d+.?\d*)'
+    re_expression = re.finditer(pattern, expr)
+
+    print(expr)
+    re_exp_list = list(re_expression)
+    expression_list = list(expr)
+    print(re_exp_list, 'es')
+    a = 0
+    s = 0
+    x, y = re_exp_list[0].span()
+    x, y = int(x) - a + s, int(y) - a + s
+    print(x, y)
+
+    re_split = re_exp_list[0][0].split()
+
+    a, act, b = re_split
     print(a, act, b)
     a = float(a)
     b = float(b)
@@ -64,5 +76,29 @@ def operations(expr):
     elif act == '/':
         c = a / b
     print(c)
+    # Зробити заміну у виразі без дужок
 
-calc("-7 + (-7 - -33) - (3 + 4)")
+    # pattern = r'(\(-?\d+.?\d* [/*+-] -?\d+.?\d*\))'
+    # re_expression = re.finditer(pattern, expr)
+    # re_exp_list = list(re_expression)
+    # expression_list = list(expr)
+    # print(re_exp_list)
+
+    # if re_exp_list != []:
+    #     print(re_exp_list[0].span())
+    #     a = 0
+    #     s = 0
+    #     x, y = re_exp_list[0].span()
+    #     x, y = int(x) - a + s, int(y) - a + s
+    #     del expression_list[x: y:]
+    #     print(expression_list, 'del')
+    #     expression_list.insert(x, str(operations_in_brackets(re_exp_list[0][0])))
+    #     print(expression_list, 'exp')
+    #     expression = ''.join(expression_list)
+
+    #     s += 1
+
+    #     print(expression, 'simpled')
+
+
+calc("-7.334 * (-7 - -4.2) * (3.333 + 4.443)")
